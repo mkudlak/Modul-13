4978579b646839bebaad0194245e8e6f400ee545
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-01-04 17:00:33 +0100

    Add base files for exe2

---
+++ zad2/js/modules/time.js
@@ -0,0 +1,30 @@
+
+process.stdin.setEncoding('utf-8');
+
+function calcTheTime () {
+    process.stdin.on('readable', function() {
+
+        var input = process.stdin.read();
+
+        if (input !== null) {
+            var instruction = input.toString().trim();
+            
+            switch (instruction) {
+                case '>=0' :
+                    console.log(dziala);
+                    break;
+
+                case '/exit':
+                    console.log('Quitting app!\n');
+                    process.exit();
+        
+                default:
+                    console.log('Wrong instruction!');
+
+            }
+        
+        
+        }
+
+    })
+}
