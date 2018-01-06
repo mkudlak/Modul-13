1297adbb25c00096ccef68e479e5666657f14f3a
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-01-06 23:59:42 +0100

    Add files for exe6 the image is  not uploading

---
+++ zad6/modules/handlers.js
@@ -0,0 +1,57 @@
+exports.upload = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania upload.");
+    response.write("Rozpoczynam upload!");
+    response.end();
+}
+
+var fs = require('fs');
+
+exports.welcome = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania welcome.");
+    fs.readFile('templates/start.html', function(err, html) {
+        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.write(html);
+        response.end();
+    });
+}
+
+exports.error = function(request, response) {
+    console.log("Nie wiem co robić.");
+    response.write("404 :(");
+    response.end();
+}
+
+var formidable = require('formidable');
+
+exports.upload = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania upload.");
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+        fs.renameSync(files.upload.path, "test.png");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
+    switch (request.url) {
+        case '/':
+        case '/start':
+            handlers.welcome(request, response);
+            break;
+        case '/upload':
+            handlers.upload(request, response);
+            break;
+        case '/show':
+            handlers.show(request, response);
+            break;
+    }
+    
+}
+
+exports.show = function(request, response) {
+    fs.readFile("test.png", "binary", function(error, file) {
+        response.writeHead(200, {"Content-Type": "image/png"});
+        response.write(file, "binary");
+        response.end();
+        })}
+
