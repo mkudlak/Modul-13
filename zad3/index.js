26633ece187b90d2b03c4c05c597999ee036c92c
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-01-06 16:13:28 +0100

    Add base files for exe3

--- zad3/index.js
+++ zad3/index.js
@@ -1,27 +1,2 @@
 var OSinfo = require('../modules/OSinfo');
 OSinfo.print();
-
-var timeCalc = require('../modules/timeCalc');
-timeCalc.print();
-
-process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-    if(input !== null) {
-        var instruction = input.trim();
-        switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-            case '/getOSinfo':
-                getOSinfo();
-                
-            break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        };
-    }
-});
