37a97416d09e09460a2493607cc633f96d388a41
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-02-28 21:50:39 +0100

    Add script in package.json to start program.js, and new script with fs module

--- zad5/program.js
+++ zad5/program.js
@@ -1,31 +1,18 @@
-/*var http = require('http');
+var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer();
-server.on('request', function (request, response) {
-    response.write('<body>');
-    response.write('<h1>Hello world!</h1>');
-    response.write('</body>');
-    response.end();
-});
-
-server.listen(9000)
-*/
-
-var http = require('http');
-
-var server = http.createServer();
-
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/hello') {
+        fs.readFile('.index.html', 'utf-8', function(err, data) {
         response.write('<h1>Hello World!</h1>');
             response.end();
+        })
     } else {
             response.statusCode = 404;
             response.write('<h1>404: Zła ścieżka!</h1>');
             response.end();
     }
 });
-
-server.listen(8080);
-
+server.listen(9000);
