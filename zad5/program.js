a45df6e1d9f783f95e2a5c12e3293a1f2c3cc9b0
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-03-02 14:44:03 +0100

    Add methos fs.readFile

--- zad5/program.js
+++ zad5/program.js
@@ -2,17 +2,24 @@ var http = require("http");
 var fs = require("fs");
 
 var server = http.createServer();
+
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/hello') {
-        fs.readFile('.index.html', 'utf-8', function(err, data) {
-        response.write('<h1>Hello World!</h1>');
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            response.write('<h1>Działa to!</h1>');
             response.end();
-        })
+        });;
     } else {
             response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
-            response.end();
+            fs.readFile('./index.html', 'utf-8', function(err, data) {
+                response.write('<h1>bla bla!</h1>');
+                response.write('<img src="web.jpg">');
+                response.end();
+            });
     }
 });
-server.listen(9000);
+
+server.listen(8080);
+
+
