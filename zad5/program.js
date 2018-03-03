d91d8560af29b5927a89dcc7604b6075d9abdb27
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-03-03 12:30:08 +0100

    Add img for the reponse

--- zad5/program.js
+++ zad5/program.js
@@ -14,7 +14,7 @@ server.on('request', function (request, response) {
             response.statusCode = 404;
             fs.readFile('./index.html', 'utf-8', function(err, data) {
                 response.write('<h1>bla bla!</h1>');
-                response.write('<img src="web.jpg">');
+                response.write('<img src="https://cdn.pixabay.com/photo/2017/03/02/16/54/iceland-2111810_1280.jpg">');
                 response.end();
             });
     }
